229e7f205549d449d9775393cd3611f25be57cd0
SmSeeley <smseeley@quinnipiac.edu>
2025-02-12 15:08:11 -0500

    added operations for multiplication and division

--- Calculator.java
+++ Calculator.java
@@ -18,17 +18,23 @@ public class Calculator {
         int num2 = scanner.nextInt();
 
         //Ask for operation
-        System.out.print("Please enter '+' or '-' to select an operation: ");
+        System.out.print("Please enter '+' or '-' or '*' or '/' to select an operation: ");
         String operation = scanner.next().toLowerCase();
 
 
         //get result
         if (operation.equals("+")) {
             result = num1 + num2;
-            System.out.println("The sum is " + result);
+            System.out.println("The total is " + result);
         } else if (operation.equals("-")) {
             result = num1 - num2;
-            System.out.println("the difference is " + result);
+            System.out.println("The total is " + result);
+        } else if (operation.equals("*")) {
+            result = num1 * num2;
+            System.out.println("The total is " + result);
+        } else if (operation.equals("/")) {
+            result = num1 / num2;
+            System.out.println("The total is " + result);
         }
 
         //close scanner
